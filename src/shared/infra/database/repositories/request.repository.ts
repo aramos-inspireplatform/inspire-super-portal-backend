f72803d063ffb2d13772d3482d2f205ec98fe033
joaodobread <joaovictorslf@gmail.com>
2023-04-12 08:11:51 -0300

    feat: add request creation

--- src/shared/infra/database/repositories/request.repository.ts
+++ src/shared/infra/database/repositories/request.repository.ts
@@ -1,15 +1,29 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Datasource, Repository } from 'typeorm';
-import { Requests } from '~/shared/infra/database/entities';
+import { DataSource, Repository } from 'typeorm';
+import { Requests, ModuleRequests } from '~/shared/infra/database/entities';
 import { IRequestRepository } from '~/requests/infra/contracts/repository/request-repository.contract';
-import {DatabaseProvidersSymbols} from '~/shared/infra/database/ioc/providers/provider.symbols'
+import { DatabaseProvidersSymbols } from '~/shared/infra/database/ioc/providers/provider.symbols';
 
 @Injectable()
 export class RequestRepository implements IRequestRepository {
   repository: Repository<Requests>;
+  moduleRequestRepository: Repository<ModuleRequests>;
 
   constructor(
     @Inject(DatabaseProvidersSymbols.DATA_SOURCE)
-    private readonly dataSource: DataSource
-  )
+    private readonly dataSource: DataSource,
+  ) {
+    this.repository = dataSource.getRepository<Requests>(Requests);
+    this.moduleRequestRepository =
+      dataSource.getRepository<ModuleRequests>(ModuleRequests);
+  }
+
+  async save(
+    attrs: IRequestRepository.SaveInputAttrs,
+  ): Promise<IRequestRepository.SaveResult> {
+    const moduleRequestEntity = this.moduleRequestRepository.create({});
+    const entity = this.repository.create({
+      ...attrs.request,
+    });
+  }
 }
