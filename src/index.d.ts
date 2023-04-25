97734b86346a78d587f44716ea1668069a489ac8
joaodobread <joaovictorslf@gmail.com>
2023-04-25 10:51:08 -0300

    fix: types definition on process.env

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,13 @@
 import { EnvironmentSchema } from '~/shared/infra/env/environment.schema';
 
-export {};
-
-interface ProcessEnv {
+interface TrashEnv {
   [key: string]: string | undefined;
 }
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv extends EnvironmentSchema, ProcessEnv {}
+    interface ProcessEnv extends EnvironmentSchema, TrashEnv {}
   }
 }
+
+export {};
