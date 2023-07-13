6dc536203bdc0ef91f31b5269d57958fedd468aa
Gabriel Dutra <gabriel.dutra@sof.to>
2023-07-13 16:36:28 -0300

    fix: modify name find-all for find-period - fix: fastifyAdapterFactory type - feat: create find-all (base) - feat: add in payouts module

--- src/payouts/ioc/providers/daos/find-period-payout-payments-dao-factory.provider.ts
+++ src/payouts/ioc/providers/daos/find-period-payout-payments-dao-factory.provider.ts
@@ -1,15 +1,15 @@
 import { FactoryProvider } from '@nestjs/common';
-import { FindAllPayoutPaymentsDao } from '~/payouts/infra/daos/find-all-payout-payments.dao';
 import { PayoutProvidersSymbols } from '~/payouts/ioc/payouts-providers.symbols';
 import { IInspirePaymentApiService } from '~/shared/application/services/inspire-api-services/payment/services/contracts/inspire-payment-api-service.contract';
+import { FindPeriodPayoutPaymentsDao } from '~/payouts/infra/daos/find-period-payout-payments.dao';
 import { InspireApiServicesProvidersSymbols } from '~/shared/application/services/inspire-api-services/shared/symbols/inspire-api-services-providers.symbols';
 
-export class FindAllPayoutPaymentsDaoFactoryProvider {
+export class FindPeriodPayoutPaymentsDaoFactoryProvider {
   static register(): FactoryProvider {
     return {
-      provide: PayoutProvidersSymbols.FIND_ALL_PAYMENTS_DAO,
+      provide: PayoutProvidersSymbols.FIND_PERIOD_PAYMENTS_DAO,
       useFactory: (inspirePaymentApiService: IInspirePaymentApiService) =>
-        new FindAllPayoutPaymentsDao(inspirePaymentApiService),
+        new FindPeriodPayoutPaymentsDao(inspirePaymentApiService),
       inject: [InspireApiServicesProvidersSymbols.INSPIRE_PAYMENT_API_SERVICE],
     };
   }
