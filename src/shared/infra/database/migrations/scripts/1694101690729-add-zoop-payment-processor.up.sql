3598fa93da25af95849f7ba7bd8c321f761380e9
Inspire Softo <inspire@sof.to>
2023-09-14 16:34:09 +0000

    fix: true for false migration

--- src/shared/infra/database/migrations/scripts/1694101690729-add-zoop-payment-processor.up.sql
+++ src/shared/infra/database/migrations/scripts/1694101690729-add-zoop-payment-processor.up.sql
@@ -1,2 +1,2 @@
 INSERT INTO processors (id, "name", is_active, integration_code, created_date, updated_date, deleted_date, is_payout_available)
-VALUES ('e56dcec7-e8ae-4670-9d7d-c8def9f92682'::uuid, 'Zoop', true, 'e56dcec7-e8ae-4670-9d7d-c8def9f92682', 'NOW()', NULL, NULL, true);
+VALUES ('e56dcec7-e8ae-4670-9d7d-c8def9f92682'::uuid, 'Zoop', true, 'e56dcec7-e8ae-4670-9d7d-c8def9f92682', 'NOW()', NULL, NULL, false);
